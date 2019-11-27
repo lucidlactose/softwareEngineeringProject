adb4636b68b13a3ea18cf43a810fc020d5997864
Lykos <javierherrera52@gmail.com>
2019-11-27 15:36:20 -0800

    added about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 const routes: Routes = [
-  {path:'', component: HomePageComponent}
+  {path:'', component: HomePageComponent},
+  {path:'aboutUs', component:AboutUsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutUsComponent,
+
   ],
   imports: [
     BrowserModule,
